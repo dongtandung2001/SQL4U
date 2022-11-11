3ee254ab90c055fc58eef70e550e579d7240de8e
Tan Dung Dong <dung.dong@sjsu.edu>
2022-11-11 14:55:35 -0800

    add login page

---
+++ frontend/src/component/common/form.jsx
@@ -0,0 +1,114 @@
+import React, { Component } from "react";
+import Joi from "joi";
+import Input from "./input";
+import Select from "./select";
+import Radio from "./radio";
+
+class Form extends Component {
+  state = {
+    data: {},
+    errors: {},
+  };
+
+  validate = () => {
+    const result = this.schema.validate(this.state.data, {
+      abortEarly: false,
+    });
+    const { error } = result;
+    if (!error) return null;
+
+    const errors = {};
+
+    for (let item of error.details) {
+      errors[item.path[0]] = item.message;
+    }
+
+    return errors;
+  };
+
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = Joi.object({
+      [name]: this.schema._ids._byKey.get(name).schema,
+    });
+
+    const { error } = schema.validate(obj);
+
+    if (!error) return null;
+    return error.details[0].message;
+  };
+
+  handleSumbit = (e) => {
+    e.preventDefault();
+
+    // validation
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+
+    if (errors) return;
+    this.doSubmit();
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
+    const data = { ...this.state.data };
+    data[input.name] = input.value;
+    this.setState({ data, errors });
+  };
+
+  renderButton(label) {
+    return (
+      <button disabled={this.validate()} className='btn btn-primary'>
+        {label}
+      </button>
+    );
+  }
+
+  renderInput(name, label, type = "text") {
+    const { data, errors } = this.state;
+    return (
+      <Input
+        name={name}
+        label={label}
+        type={type}
+        value={data[name]}
+        error={errors[name]}
+        onChange={this.handleChange}
+      />
+    );
+  }
+
+  renderSelect(name, label, options) {
+    const { data, errors } = this.state;
+    return (
+      <Select
+        name={name}
+        label={label}
+        value={data[name]}
+        options={options}
+        error={errors[name]}
+        onChange={this.handleChange}
+      />
+    );
+  }
+
+  renderRadio(name, inputs) {
+    const { data, errors } = this.state;
+    return (
+      <Radio
+        name={name}
+        value={data[name]}
+        error={errors[name]}
+        inputs={inputs}
+        onChange={this.handleChange}
+      />
+    );
+  }
+}
+
+export default Form;
--- frontend/src/component/login/login.jsx
+++ frontend/src/component/login/login.jsx
@@ -1,9 +1,59 @@
-import React, { Component } from "react";
+import React from "react";
+import Joi from "joi";
 
-class Login extends Component {
-  state = {};
+import Form from "../common/form";
+class Login extends Form {
+  state = {
+    data: {
+      username: "",
+      password: "",
+      skill: "",
+    },
+    skills: [
+      { name: "beginner", label: "Beginner" },
+      { name: "intermediate", label: "Intermediate" },
+      { name: "professional", label: "Professional" },
+    ],
+    errors: {},
+  };
+
+  schema = Joi.object({
+    username: Joi.string().min(5).max(50).required().label("Username"),
+    password: Joi.string().min(5).max(255).required().label("Password"),
+    skill: Joi.string().required(),
+  });
+
+  doSubmit = () => {
+    console.log("Login button called", this.state.data);
+  };
   render() {
-    return <h1>Login</h1>;
+    return (
+      <div className='container-fluid'>
+        <div
+          style={{ background: "#68BBE3" }}
+          className='row text-center shadow-sm'
+        >
+          <div
+            style={{
+              background: "#F2F3F1",
+              minHeight: "85vh",
+            }}
+            className='col-4 flex-column d-flex justify-content-center align-items-center shadow rounded'
+          >
+            <i className='fa fa-user-o fa-5x'></i>
+            <h2>Let's start to learn!</h2>
+            <form onSubmit={this.handleSumbit}>
+              {this.renderInput("username", "UserName")}
+              {this.renderInput("password", "Password")}
+              <p className='mt-3'>How strong is your database skill</p>
+              {this.renderRadio("skill", this.state.skills)}
+              {this.renderButton("Create Account")}
+            </form>
+          </div>
+          <div className='col'></div>
+        </div>
+      </div>
+    );
   }
 }
 
