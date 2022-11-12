f3061edfb4f708ad4ccb35898ad46e739943b33d
Tan Dung Dong <dung.dong@sjsu.edu>
2022-11-11 16:34:03 -0800

    Setup backend directoires and packages

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-console.log("backend");
+const { restart } = require("nodemon");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ message: "Welcome to SQL4U API" });
+});
+
+const port = process.env.PORT || 3900;
+app.listen(port, () => console.log(`Listening on port ${port}`));
