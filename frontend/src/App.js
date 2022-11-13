616569c91a4d8f628aeec30b7ef10deef803cdc4
Dung Dong <dongtandung2001@gmail.com>
2022-11-12 18:47:28 -0800

    Finish login/logout/signup feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,12 +17,19 @@ import Logout from "./component/logout/logout";
 import Profile from "./component/profile/profile";
 import NotFound from "./component/not-found/notFound";
 
+import auth from "./services/authService";
+
+
 class App extends Component {
   state = {};
+  async componentDidMount() {
+    const user = auth.getCurrentUser();
+    this.setState({ user });
+  }
   render() {
     return (
       <main className='container'>
-        <NavBar />
+        <NavBar user={this.state.user} />
         {this.state.user && <FeatureNavBar />}
 
         <Routes>
