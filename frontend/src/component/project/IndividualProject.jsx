7917660593e9dcba553199293a52859cc5c13242
Dung Dong <dongtandung2001@gmail.com>
2022-11-16 01:49:22 -0800

    Change url for project and fix some reactDOM bug in dashboard

--- frontend/src/component/project/IndividualProject.jsx
+++ frontend/src/component/project/IndividualProject.jsx
@@ -1,51 +1,49 @@
-import React, {useEffect, useState} from "react";
-import { BsFillCheckCircleFill } from 'react-icons/bs';
-import {AiFillDelete} from 'react-icons/ai';
-import { Link, useLocation } from 'react-router-dom';
-import './recommendedProjects.css';
+import React, { useEffect, useState } from "react";
+import { BsFillCheckCircleFill } from "react-icons/bs";
+import { AiFillDelete } from "react-icons/ai";
+import { Link, useLocation } from "react-router-dom";
+import "./recommendedProjects.css";
 import auth from "../../services/authService";
 
 export default function IndividualProject(props) {
   const location = useLocation();
-  console.log('hi nha', location);
-  const {courseId} = location.state;
+  console.log("hi nha", location);
+  const { courseId } = location.state;
   const [state, setState] = useState({});
 
-  useEffect( () => {
+  useEffect(() => {
     const user = auth.getCurrentUser();
-    setState(user)
+    setState(user);
   }, []);
-  
+
   const thisProject = {
     title: props.title,
-    content: props.content
-  }
-
+    content: props.content,
+  };
 
   return (
-    <div className='project-title-div'>
+    <div className="project-title-div">
       <Link
-        to={`/catalog/${courseId}/project/projectpage/${props.id+1}`}
+        to={`/catalog/${courseId}/project/${props.id + 1}`}
         state={{ content: thisProject }}
-        className='project-title'
+        className="project-title"
       >
-        <span style={{ marginRight: "10px" }}>{props.id+1}.</span>
+        <span style={{ marginRight: "10px" }}>{props.id + 1}.</span>
         {props.title}
       </Link>
-      {state.admin ? 
-        <button 
-          type='button' 
+      {state.admin ? (
+        <button
+          type="button"
           className="check-button"
-          onClick={e => props.handleDelete(props.id, e)}
+          onClick={(e) => props.handleDelete(props.id, e)}
         >
-          <AiFillDelete className='check-icon' />
+          <AiFillDelete className="check-icon" />
         </button>
-      :
-      <button type='button' className="check-button" >
-          <BsFillCheckCircleFill className='check-icon'  />
+      ) : (
+        <button type="button" className="check-button">
+          <BsFillCheckCircleFill className="check-icon" />
         </button>
-      
-      }
+      )}
     </div>
   );
 }
