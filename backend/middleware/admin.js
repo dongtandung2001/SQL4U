4598f14763420f660cd4c1bec5cfe16171e81e3b
Tan Dung Dong <dung.dong@sjsu.edu>
2022-11-25 12:56:17 -0800

    Update admin middleware

--- backend/middleware/admin.js
+++ backend/middleware/admin.js
@@ -1,4 +1,4 @@
 module.exports = function admin(req, res, next) {
-    if (!req.user.isAdmin) return res.status(403).send("Access Denied");
-    next();
-}
+  if (!req.user.admin) return res.status(403).send("Access Denied");
+  next();
+};
