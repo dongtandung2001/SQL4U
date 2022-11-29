65ed3899e718ee65af4ac1eda7b7abcb0bff7fd5
Tan Dung Dong <dung.dong@sjsu.edu>
2022-11-29 13:22:05 -0800

    Add route for saving interview questions and finished projects

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,52 +1,68 @@
-const Joi = require('joi');
-const mongoose = require('mongoose');
-const config = require('config');
-const jwt = require('jsonwebtoken');
-
+const Joi = require("joi");
+const mongoose = require("mongoose");
+const config = require("config");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
-    email: {
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+  },
+  admin: {
+    type: Boolean,
+    default: false,
+  },
+  skill: {
+    type: String,
+  },
+  finishedProject: [
+    new mongoose.Schema({
+      title: {
         type: String,
         required: true,
-        minlength: 5,
-        maxlength: 255,
-        unique: true,
-    },
-    password: {
+      },
+    }),
+  ],
+  savedQuestion: [
+    new mongoose.Schema({
+      question: {
         type: String,
         required: true,
-        minlength: 5,
-        maxlength: 255,
-    },
-    admin: {
-        type: Boolean,
-        default: false,
-    },
-    skill: {
-        type: String,
-    }
-})
+      },
+    }),
+  ],
+});
 
 userSchema.methods.generateJwtToken = function () {
-    const token = jwt.sign(
-        {
-            _id: this._id,
-            admin: this.admin,
-            email: this.email,
-        }, config.get("jwtPrivateKey")
-    )
-    return token;
-}
+  const token = jwt.sign(
+    {
+      _id: this._id,
+      admin: this.admin,
+      email: this.email,
+    },
+    config.get("jwtPrivateKey")
+  );
+  return token;
+};
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 function validateUser(user) {
-    const schema = Joi.object({
-        email: Joi.string().min(5).max(255).required().email(),
-        password: Joi.string().min(5).max(255).required(),
-        skill: Joi.string(),
-    })
-    return schema.validate(user);
+  const schema = Joi.object({
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required(),
+    skill: Joi.string(),
+  });
+  return schema.validate(user);
 }
 
 exports.User = User;
